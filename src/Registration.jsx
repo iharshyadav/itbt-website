79bff894b3ad19746a84be925b0e3ec4e0fd9aa6
Harsh Yadav <hy945196@gmail.com>
2023-12-01 13:44:26 +0530

    changes

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -1,10 +1,92 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 const Registration = () => {
   return (
     <>
       <Navbar/>
+      <div className="bg-gray-200">
+      <div className="container mx-auto p-5">
+        <div className="text-center">
+          <h2 className="text-4xl font-bold text-indigo-800">Registration</h2>
+          <div className="border-b-4 border-indigo-800 w-1/4 mx-auto my-3"></div>
+        </div>
+
+        <div className="my-8">
+          <div className="reg-main">
+            <h3 className="text-indigo-800">Please Read</h3>
+            <p className="text-lg">
+              Note: Minimum one registration is mandatory for the paper to be
+              part of proceedings. If the author/co-author wishes to attend the
+              conference and requires separate proceedings and a certificate
+              along with the conference kit, in that case, the author/co-author
+              needs to pay full registration fees individually. Accommodation
+              could be arranged in the guest house of Ajay Kumar Garg
+              Engineering College or nearby hotels as per availability, on a
+              payment basis for which the organizers will provide necessary
+              assistance if informed well in advance.
+            </p>
+
+            <h3 className="text-indigo-800">The fees structure is as follows:</h3>
+          </div>
+        </div>
+
+        <div className="reg-table">
+          <table className="table table-striped w-95 h-400 p-10 text-lg shadow-md">
+            <thead>
+              <tr>
+                <th className="text-center align-middle font-bold">Category</th>
+                <th className="text-center align-middle font-bold">
+                  Indian authors (INR)
+                </th>
+                <th className="text-center align-middle font-bold">
+                  International authors (USD)
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="text-center align-middle font-bold">
+                  Scholars/Students
+                </td>
+                <td className="text-center align-middle">₹4000</td>
+                <td className="text-center align-middle">$200</td>
+              </tr>
+              <tr>
+                <td className="text-center align-middle font-bold">Academia</td>
+                <td className="text-center align-middle">₹6000</td>
+                <td className="text-center align-middle">$300</td>
+              </tr>
+              <tr>
+                <td className="text-center align-middle font-bold">Industry</td>
+                <td className="text-center align-middle">₹8000</td>
+                <td className="text-center align-middle">$300</td>
+              </tr>
+              <tr>
+                <td className="text-center align-middle font-bold">
+                  Attendees Only
+                </td>
+                <td className="text-center align-middle">₹5000</td>
+                <td className="text-center align-middle">$100</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+
+        <div className="reg-note text-lg mt-12">
+          <h5 className="text-indigo-800">Note:</h5>
+          <p>
+            Registration fee does not include accommodation and transportation
+            expenses. <br />
+            After Payment, please send the transaction receipt and details to
+            the following Google form link:
+          </p>
+        </div>
+      </div>
+    </div>
+    <Footer/>
     </>
   )
 }
--- src/Venues.jsx
+++ src/Venues.jsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import akshardham from '../public/Images/venue1.jpg'
+import indiagate from'../public/Images/venue2.jpg'
+import  redfort from '../public/Images/venue3.jpg'
+import Navbar from './Components/Navbar'
 
 const Venues = () => {
   return (
-    <div>Venues</div>
+    <>
+    <Navbar/>
+           <div id="venue" className="h-screen">
+      <div className="container">
+        <h1 className="text-5xl font-bold mt-5 text-center text-purple-800">Venue</h1>
+        <hr className="w-1/4 mx-auto my-6 border-t-4 border-yellow-500" />
+        <div className="text-center mb-8">
+              <p className="text-xl">ICLLM -2024 will take place in AKGEC Main seminar hall.</p>
+            </div>
+        <div className="flex">
+          <div>
+           
+          </div>
+          <div className="flex flex-wrap gap-4">
+            <div className='w-full sm:w-1/2 p-8'>
+              <div className="text-center w-3/4 sm:w-full">
+                <h5 className="text-3xl sm:ml-8  mb-8 underline font-semibold mt-12">About Ghaziabad</h5>
+                <p className="text-lg sm:ml-28 leading-7 text-justify">
+                  Ghaziabad, located in the Indian state of Uttar Pradesh, is a <br /> significant city in the National Capital Region (NCR) of India. <br /> It is an industrial hub known for manufacturing industries and is well-connected to Delhi through road and metro networks. <br /> The city has a rich history, numerous educational institutions, and landmarks like Swarna Jayanti Park and ISKCON Temple.
+                </p>
+              </div>
+              <div className="text-center">
+                <a href="#myCarousel" className="btn btn-primary mt-4">Tourist Attractions</a>
+              </div>
+            </div>
+            <div className="formMap sm:w-44 w-76 p-6">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.467007802247!2d77.49981541461779!3d28.67567358240077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf30885b1e2a5%3A0x9983675e24c6638b!2sAKGEC%3A%20Ajay%20Kumar%20Garg%20Engineering%20College%2C%20Ghaziabad!5e0!3m2!1sen!2sin!4v1676826055930!5m2!1sen!2sin"
+                width="350"
+                height="450"
+                title="location"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="carouselSection  flex flex-wrap items-center justify-center">
+        <div className="carouselContainer ">
+          <div className="row justify-center align-center">
+            <div className=" justify-center align-center w-full flex flex-col sm:flex-row gap-3 mt-24 ">
+              <div className="card w-full flex flex-wrap " style={{ width: '23rem', height: '37rem' }} >
+                <div className="card-body  ">
+                  <img src={akshardham} className=" rounded-md  card-img-top" alt="dummyImg"  />
+                  <h5 className="card-title mt-4">Akshardham Temple</h5>
+                  <p className="card-text">
+                    Akshardham, a spectacular temple complex located in Delhi, is a must-visit tourist destination for those in the Ghaziabad area. Just a short drive away, it offers a serene and culturally enriching experience. The intricately carved architecture, exhibitions, and lush gardens make it a perfect place to spend a day exploring Indian spirituality and history.
+                  </p>
+                </div>
+              </div>
+              <div className="card item" style={{ width: '23rem', height: '37rem' }}>
+                <div className="card-body  ">
+                  <img src={indiagate} className=" rounded-md  card-img-top" alt="dummyImg"  />
+                  <h5 className="card-title mt-4">India Gate</h5>
+                  <p className="card-text">
+                    India Gate, a war memorial in the heart of Delhi, is easily accessible from Ghaziabad. It's a revered monument that pays homage to the nation's fallen soldiers. The vast lawns surrounding the monument provide an excellent spot for picnics and leisurely strolls, making it a popular destination for people from Ghaziabad and nearby areas.
+                  </p>
+                </div>
+              </div>
+              <div className="card item" style={{ width: '23rem', height: '37rem' }}>
+                <div className="card-body ">
+                  <img src={redfort} className="  h-52 w-96 rounded-md card-img-top" alt="dummyImg"  />
+                  <h5 className="card-title mt-4">Red Fort</h5>
+                  <p className="card-text  w-74">
+                    The Red Fort, a UNESCO World Heritage Site, is a short drive from Ghaziabad. This historic gem in Old Delhi is known for its stunning red sandstone architecture and rich Mughal history. A visit to the Red Fort is a journey back in time, offering a glimpse into India's glorious past.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='h-16'></div>
+    </div>
+    </>
   )
 }
 
